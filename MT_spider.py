99b8a55e393126a4fef673bee3c4d357f8ea5dab
Arics <51198635+AricsHuang@users.noreply.github.com>
2021-01-16 12:06:47 +0800

    Update MT_spider.py

--- MT_spider.py
+++ MT_spider.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/15 12:27
+# @Author  : Arics
+# @Email   : aricshuang@qq.com
+# @File    : main.py.py
+# @Software: PyCharm
+
 import execjs
 import os
 import requests
@@ -10,7 +18,6 @@ import time
 # 获取商铺信息
 def get_shop_info(session, wm_latitude=22634767, wm_longitude=113834247):
     """
-
     :param wm_longitude: 定位的经度
     :param wm_latitude: 定位的纬度
     :param session: 保持会话的实例
@@ -42,19 +49,23 @@ def get_shop_info(session, wm_latitude=22634767, wm_longitude=113834247):
     }
     url = "https://i.waimai.meituan.com/openh5/homepage/poilist?_={}".format(execjs.eval("Date.now()"))
     print(f"当前爬取坐标({data['wm_longitude']},{data['wm_latitude']})")
+    allInfo = []
     try:
         # 只爬取第四页，后续数据需要登录才能获取
-        for index in range(4):
+        for index in range(2):
+        # while(True):
             res = session.post(url=url, headers=headers, data=data, timeout=5)
 
+            print(res.text)
             # 返回数据成功
             if res.status_code == 200:
                 shop_list = json.loads(res.text).get("data").get("shopList")
-                return shop_list
+                # return shop_list
+                allInfo.append(shop_list)
             else:
                 print("get_shop_info：返回数据失败，status_code非200")
             start_index += 1  # 页数加1
-            time.sleep(2)
+            # time.sleep(2)
 
     except exceptions.ConnectionError:
             print("get_shop_info：网络连接错误")
@@ -62,6 +73,7 @@ def get_shop_info(session, wm_latitude=22634767, wm_longitude=113834247):
     except exceptions.Timeout:
             print("get_shop_info：超过等待时间")
 
+    return allInfo
 
 # 保存商铺信息
 def save_shop_info(path, data, name='shopInfo.xls'):
@@ -78,11 +90,12 @@ def save_shop_info(path, data, name='shopInfo.xls'):
 
     # Excel写入数据
     dict_key = ['mtWmPoiId', 'shopName', 'wmPoiScore', 'monthSalesTip', 'address']  # 需要写入的key值
-    for h in range(len(data)):
-        _dict = data[h]
-        for l in range(len(dict_key)):
-            content = _dict.get(dict_key[l])
-            sheet.write(h+1, l, content)
+    for info in range(len(data)):
+        for h in range(len(data[info])):
+            _dict = data[info][h]
+            for l in range(len(dict_key)):
+                content = _dict.get(dict_key[l])
+                sheet.write(h+1, l, content)
 
     # 保存Excel
     book.save(filepath)
@@ -92,47 +105,48 @@ def save_shop_info(path, data, name='shopInfo.xls'):
 def get_food_info(shop_list, session):
     data_list = []
     # 遍历所有店铺
-    for _dict in shop_list:
-        shop_id = _dict.get("mtWmPoiId")
-        headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Origin': 'https://h5.waimai.meituan.com',
-            'Referer': 'https://h5.waimai.meituan.com/waimai/mindex/menu?dpShopId=&mtShopId={}&source=shoplist'.format(shop_id),
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Mobile Safari/537.36',
-            'Cookie': "lxsdk_cuid=16915455dbec8-08db788af7a2ff-1333063-1fa400-16915455dbec8; ci=30; _ga=GA1.3.2111621561.1550840081; _gid=GA1.3.426525714.1550840081; IJSESSIONID=1oa4mjr7l5r0c1fa32alvdcpbn; iuuid=C2540F1F12DE8DEB7EFFE84661C401CBEDED6125E30E08F9BF648F828AD42BDF; cityname=%E6%B7%B1%E5%9C%B3; _lxsdk=C2540F1F12DE8DEB7EFFE84661C401CBEDED6125E30E08F9BF648F828AD42BDF; webp=1; ci3=1; _hc.v=f8c27eb3-603c-e958-93d8-63f0bfa0a746.1550840270; __utmz=74597006.1550906260.3.3.utmcsr=meishi.meituan.com|utmccn=(referral)|utmcmd=referral|utmcct=/i/; latlng=22.636802,113.829362,1550906262992; i_extend=C_b1Gimthomepagecategory1394H__a; openh5_uuid=C2540F1F12DE8DEB7EFFE84661C401CBEDED6125E30E08F9BF648F828AD42BDF; showTopHeader=show; _lxsdk_s=1691935afe6-838-f5a-f2%7C%7C44; _lx_utm=utm_source%3D60030; __utma=74597006.2078366826.1550840256.1550901561.1550906260.3; __utmc=74597006; wm_order_channel=mtib; __mta=51223190.1550840082094.1550840082094.1550840111070.2"
-        }
-        data = {
-            'geoType': '2',
-            'mtWmPoiId': shop_id,
-            'dpShopId': '-1',
-            'source': 'shoplist',
-            'skuId': '',
-            '_token': '',
-        }
-        # 拼接API接口
-        url = "https://i.waimai.meituan.com/openh5/poi/food?_={}".format(execjs.eval("Date.now()"))
+    for shop in shop_list:
+        for _dict in shop:
+            shop_id = _dict.get("mtWmPoiId")
+            headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Origin': 'https://h5.waimai.meituan.com',
+                'Referer': 'https://h5.waimai.meituan.com/waimai/mindex/menu?dpShopId=&mtShopId={}&source=shoplist'.format(shop_id),
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Mobile Safari/537.36',
+                'Cookie': "lxsdk_cuid=16915455dbec8-08db788af7a2ff-1333063-1fa400-16915455dbec8; ci=30; _ga=GA1.3.2111621561.1550840081; _gid=GA1.3.426525714.1550840081; IJSESSIONID=1oa4mjr7l5r0c1fa32alvdcpbn; iuuid=C2540F1F12DE8DEB7EFFE84661C401CBEDED6125E30E08F9BF648F828AD42BDF; cityname=%E6%B7%B1%E5%9C%B3; _lxsdk=C2540F1F12DE8DEB7EFFE84661C401CBEDED6125E30E08F9BF648F828AD42BDF; webp=1; ci3=1; _hc.v=f8c27eb3-603c-e958-93d8-63f0bfa0a746.1550840270; __utmz=74597006.1550906260.3.3.utmcsr=meishi.meituan.com|utmccn=(referral)|utmcmd=referral|utmcct=/i/; latlng=22.636802,113.829362,1550906262992; i_extend=C_b1Gimthomepagecategory1394H__a; openh5_uuid=C2540F1F12DE8DEB7EFFE84661C401CBEDED6125E30E08F9BF648F828AD42BDF; showTopHeader=show; _lxsdk_s=1691935afe6-838-f5a-f2%7C%7C44; _lx_utm=utm_source%3D60030; __utma=74597006.2078366826.1550840256.1550901561.1550906260.3; __utmc=74597006; wm_order_channel=mtib; __mta=51223190.1550840082094.1550840082094.1550840111070.2"
+            }
+            data = {
+                'geoType': '2',
+                'mtWmPoiId': shop_id,
+                'dpShopId': '-1',
+                'source': 'shoplist',
+                'skuId': '',
+                '_token': '',
+            }
+            # 拼接API接口
+            url = "https://i.waimai.meituan.com/openh5/poi/food?_={}".format(execjs.eval("Date.now()"))
 
-        # 针对网络问题、请求超时、数据返回状态进行异常捕获
-        try:
-            response = session.post(url=url, headers=headers, data=data, timeout=5)
+            # 针对网络问题、请求超时、数据返回状态进行异常捕获
+            try:
+                response = session.post(url=url, headers=headers, data=data, timeout=5)
 
-            # 是否返回成功
-            if response.status_code == 200:
-                _data = json.loads(response.text)
+                # 是否返回成功
+                if response.status_code == 200:
+                    _data = json.loads(response.text)
 
-                # 是否返回正确数据
-                if _data.get("msg") == "成功":
-                    data_dict = _data.get("data")
-                    data_list.append(data_dict)
-            else:
-                print("get_food_info：status_code非200")
-        except exceptions.ConnectionError:
-            print("get_food_info：网络连接错误")
-        except exceptions.Timeout:
-            print("get_food_info：超过等待时间")
-        print("*" * 30 + "【{}】商品信息保存完毕".format(_dict.get("shopName")) + "*" * 30)
-        time.sleep(2)
+                    # 是否返回正确数据
+                    if _data.get("msg") == "成功":
+                        data_dict = _data.get("data")
+                        data_list.append(data_dict)
+                else:
+                    print("get_food_info：status_code非200")
+            except exceptions.ConnectionError:
+                print("get_food_info：网络连接错误")
+            except exceptions.Timeout:
+                print("get_food_info：超过等待时间")
+            print("*" * 30 + "【{}】商品信息保存完毕".format(_dict.get("shopName")) + "*" * 30)
+            time.sleep(2)
 
     return data_list
 
@@ -189,5 +203,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
